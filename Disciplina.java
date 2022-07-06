08a39a018d41e614c7e68d3d29ef154c936c487b
VitorComin <106283734+VitorComin@users.noreply.github.com>
2022-07-06 20:57:03 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,37 @@
+package Trabalho;
+
+import java.util.*;
+
+public class Disciplina {
+    private String nome;
+    private List<Aluno> alunos;
+    private List<Notas> Notas;
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public List<Notas> getNotas() {
+        return Notas;
+    }
+
+    public void setComposicaoNotas(List<CNotas> Notas) {
+        this.Notas = Notas;
+    }
+}
